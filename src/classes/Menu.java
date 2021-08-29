80e9bd68a04e6bbe74e912a6bac615529d23cc6a
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2021-08-29 00:45:10 -0500

    Create Menu types methods

--- src/classes/Menu.java
+++ src/classes/Menu.java
@@ -6,13 +6,14 @@ public class Menu {
     
     public static int option;
     public static Scanner scanner = new Scanner(System.in);
+    public static Airplane airplane = new Airplane();
 
     public Menu() {
-        startMenu();
+        mainMenu();
     }
 
-    public static void startMenu() {
-        
+    public static void mainMenu() {
+
         do {
             System.out.println("\nMENU");
             System.out.println("1. Request seat");
@@ -20,22 +21,81 @@ public class Menu {
             System.out.println("3. Get out");
             System.out.print("Enter an option: ");
             option = scanner.nextInt();
+            System.out.println();
 
             switch (option) {
                 case 1:
-                    System.out.println("Request seat");
+                    requestSeatMenu();
                     break;
                 case 2:
-                    System.out.println("Check seat availability");
+                    airplane.printSeats();
                     break;
-                case 3:
-                    System.out.println("See you!\n");
-                    return;                
                 default:
-                    break;
+                    System.out.println("See you!\n");
+                    return;
             }
-        } while (option>= 1 && option <= 3);
+        } while (option>= 1 && option <= 2);
 
+    }
+
+    public static void requestSeatMenu() {
+        String name;
+        String ssn;
+        int seatClass;
+        
+        scanner.nextLine();
+        System.out.print("Enter name: ");
+        name = scanner.nextLine();
+        System.out.print("Enter SSN: ");
+        ssn = scanner.nextLine();
+        
+        Passenger passenger = new Passenger(name, ssn);
+                
+        do {
+            scanner.nextLine();
+            System.out.println("Select class");
+            System.out.println("1. Executive");
+            System.out.println("2. Economic");
+            System.out.print("Enter an option: ");
+            seatClass = scanner.nextInt();
+
+            if (seatClass> 1 && seatClass < 2) {
+                System.out.println("Invalid option");
+            }
+
+        } while (seatClass> 1 && seatClass < 2);
+        
+        selectLocationMenu(seatClass);
+        
+    }
+    
+    public static void selectLocationMenu(int seatClass) {
+        
+        int seatNumber;
+
+        airplane.printSeats();
+        
+        switch (seatClass) {
+            case 1:
+                scanner.nextLine();
+                System.out.println("Select location");
+                System.out.println("1. Window");
+                System.out.println("2. Hall");
+                System.out.print("Enter an option: ");
+                seatNumber = scanner.nextInt();
+                break;
+            case 2:
+                scanner.nextLine();
+                System.out.println("Select location");
+                System.out.println("1. Window");
+                System.out.println("2. Center");
+                System.out.println("3. Hall");
+                System.out.print("Enter an option: ");
+                seatNumber = scanner.nextInt();
+                break;
+            default:
+                break;
+        }
     }
 
 
